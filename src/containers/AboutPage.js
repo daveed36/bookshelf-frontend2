555cb215b5fabd642dcf1fe8720767b6f82bd4b5
Maher George Brikae <mahergeorgebrikae@Mahers-MacBook-Pro.local>
2021-02-12 18:00:43 -0500

    frontend push

--- src/containers/AboutPage.js
+++ src/containers/AboutPage.js
@@ -9,7 +9,7 @@ const AboutPage = () => {
       </div>
       <main>
         <div className="about">
-          <p>BookShelf is an application designed for readers to find and save books, set reading goals, and track their progress. It is built with a React.js frontend and Ruby on Rails backend. Book data is gathered from the New York Times Bestsellers API and the Google Books API.</p>
+          <p>BookShelf is an application designed for readers to find and save books, set reading goals, and track their progress. It is built with a React.js frontend and Ruby on Rails backend. Book data is gathered from the Google Books API.</p>
         </div>
       </main>
     </>
