7717e4a2f8e47d65bf52df7c199b317dbb6e8c68
Maher George Brikae <mahergeorgebrikae@Mahers-MacBook-Pro.local>
2021-02-16 12:48:55 -0500

    contact addedww1

--- src/containers/SignInPage.js
+++ src/containers/SignInPage.js
@@ -76,7 +76,7 @@ export default class SignInPage extends Component {
         localStorage.setItem("token", response.token)
         localStorage.setItem("userId", response.user_id)
         this.props.setUser(response.user_id)
-        window.location.href = "http://localhost:3001/"
+        window.location.href = "https://bookshelf-frontend2.netlify.app"
       })
       .catch(error => console.log(error))
   }
